c50e8ef84299219cca46e986d1f004059e274fe7
yandongli <1411707895@qq.com>
2020-06-26 16:03:48 +0800

    liyandong|>code resolution

--- liyandong/Kotlin/customview/src/main/java/com/example/customview/SpottedLinearLayout.java
+++ liyandong/Kotlin/customview/src/main/java/com/example/customview/SpottedLinearLayout.java
@@ -22,11 +22,6 @@ public class SpottedLinearLayout extends LinearLayout {
 
     public SpottedLinearLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
         paint = new Paint();
         paint.setColor(Color.GREEN);
         paint1 = new Paint();
@@ -35,10 +30,15 @@ public class SpottedLinearLayout extends LinearLayout {
         paint2.setColor(Color.RED);
         paint3 = new Paint();
         paint3.setColor(Color.MAGENTA);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+
         canvas.drawCircle(300, 300, 10, paint);
         canvas.drawCircle(400, 350, 22, paint1);
         canvas.drawCircle(500, 450, 50, paint2);
         canvas.drawCircle(700, 600, 35, paint3);
-
     }
 }
--- liyandong/Kotlin/customview/src/main/java/com/example/customview/auxiliaryView.java
+++ liyandong/Kotlin/customview/src/main/java/com/example/customview/auxiliaryView.java
@@ -1,5 +1,6 @@
 package com.example.customview;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,21 +15,26 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
-public class auxiliaryView extends View {
+public class AuxiliaryView extends View {
 
     private Paint mPaint;
     private Bitmap imageBitmap;
     private Path path;
     private float[] rids = new float[]{10.0f, 10.0f, 10.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f,};
+    private Camera camera;
+    private Matrix matrix;
+    private ValueAnimator animator;
+    private float degree;
 
-    public auxiliaryView(Context context) {
+    public AuxiliaryView(Context context) {
         super(context);
-        mPaint = new Paint();
     }
 
-    public auxiliaryView(Context context, @Nullable AttributeSet attrs) {
+    public AuxiliaryView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mPaint = new Paint();
+        camera = new Camera();
+        matrix = new Matrix();
         imageBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.yh0);
     }
 
@@ -62,16 +68,34 @@ public class auxiliaryView extends View {
 //        canvas.concat(matrix);  //调用
         // 使用setPolyToPoly 自定义变化
         //使用Camera 进行三维变换   Camera 也需要 保存和恢复状态才能正常绘制
-        Camera camera = new Camera();
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
         canvas.save();
         camera.save();
-        camera.rotateX(20); // 旋转 Camera 的三维空间  x轴旋转30度
-        canvas.translate(getWidth() / 2, getHeight() / 2); // 旋转之后把投影移动回来
-        camera.applyToCanvas(canvas); // 把旋转投影到 Canvas
-        canvas.translate(-getWidth() / 2, -getHeight() / 2); // 旋转之前把绘制内容移动到轴心（原点）
-        camera.setLocation(0, 0, 50f);
+        camera.rotateX(degree); // 旋转 Camera 的三维空间  x轴旋转30度
+        int width = imageBitmap.getWidth();
+        int height = imageBitmap.getHeight();
+        matrix.postScale(measuredWidth / width, measuredHeight / height);
+        camera.getMatrix(matrix);
+//        canvas.translate(measuredWidth / 2, measuredHeight / 2); // 旋转之后把投影移动回来
+//        camera.applyToCanvas(canvas); // 把旋转投影到 Canvas
+//        canvas.translate(-measuredWidth / 2, measuredHeight / 2); // 旋转之前把绘制内容移动到轴心（原点）
+//        camera.setLocation(0, 0, 50f);
         camera.restore();
-        canvas.drawBitmap(imageBitmap, 150, 50, mPaint);
+        canvas.drawBitmap(imageBitmap, matrix, mPaint);
         canvas.restore();
+    }
+
+    public void startAnimation() {
+        animator = ValueAnimator.ofFloat(0, 90);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                degree = (float) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        animator.setDuration(2000);
+        animator.start();
     }
 }
