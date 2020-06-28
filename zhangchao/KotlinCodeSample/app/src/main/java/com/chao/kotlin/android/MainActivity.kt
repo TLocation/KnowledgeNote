9f123db0fb4d26b7dfcaf3121d638ee123e6d115
ZhangChao <18833628372@163.com>
2020-06-28 23:01:33 +0800

    new create kotlin project

---
+++ zhangchao/KotlinCodeSample/app/src/main/java/com/chao/kotlin/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.chao.kotlin.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chao.kotlin.code.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
